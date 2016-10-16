48325868b9f846022462027f28c46020b68b653d
Brad Lucas <brad@beaconhill.com>
2016-10-15 21:44:44 -0400

    version 1.8.0 of clojure

--- the-divine-cheese-code/project.clj
+++ the-divine-cheese-code/project.clj
@@ -1,7 +1,7 @@
-(defproject the-divine-cheese-code "0.1.0-SNAPSHOT"
+(defproject the-divine-cheese-code "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main the-divine-cheese-code.core)
